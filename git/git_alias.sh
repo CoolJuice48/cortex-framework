1720ca8bab82264891bd2636193b02d687260d5e
Christopher Taylor <taylojt@umich.edu>
2026-01-26 01:28:38 -0500

    Update: 2026-01-26 01:28

---
+++ git/git_alias.sh
@@ -0,0 +1,4 @@
+alias gpush='~/Documents/Favorites/Cortex_v1/git/git_push.sh'
+alias gstat='~/Documents/Favorites/Cortex_v1/git/git_status.sh'
+alias gsync='~/Documents/Favorites/Cortex_v1/git/git_sync.sh'
+alias gquick='~/Documents/Favorites/Cortex_v1/git/git_quick.sh'
